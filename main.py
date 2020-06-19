52ffffcccb116cec29dbd62f0d8a8451c81bef14
Caoimh <55163829+CaoimhConway@users.noreply.github.com>
2020-06-19 12:52:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,35 @@
-# Use 5 ETFs (SPY - US stocks, EFA - foreign stocks, IEF - bonds, VNQ - REITs, GSG - commodities).
-# Pick 3 ETFs with strongest 12 month momentum into your portfolio and weight them equally. 
-# Hold for 1 month and then rebalance.
-
-import pandas as pd
 from datetime import datetime
+from collections import *
 
-class AssetClassMomentumAlgorithm(QCAlgorithm):
 
+
+class AllWeatherStrategy(QCAlgorithm):
     def Initialize(self):
-
         self.SetStartDate(2000, 1, 1)  
-        self.SetEndDate(datetime.now())  
-        self.SetCash(100000)
+        self.SetEndDate(2020, 1, 31)  
+        self.SetCash(100000) 
+        self.monthCounter = 0
         
         
-        self.data = {}
-        period = 12*21
-        self.symbols = ["SPY", "EFA", "IEF", "VNQ", "GSG"]
-        
-        self.SetWarmUp(period)
-        
-        for symbol in self.symbols:
-            self.AddEquity(symbol, Resolution.Daily)
-            self.data[symbol] = self.MOM(symbol, period, Resolution.Daily)
-         
-        self.Schedule.On(self.DateRules.MonthStart("SPY"), self.TimeRules.AfterMarketOpen("SPY"), self.Rebalance)
+        # Country indexes
+        self.etfs = [
+            (self.AddEquity('VTI', Resolution.Daily).Symbol,0.3), #Vanguard Total Stock Market ETF
+            (self.AddEquity('TLT', Resolution.Daily).Symbol,0.4), # iShares 20+ Year Treasury ETF (TLT)
+            (self.AddEquity('IEF', Resolution.Daily).Symbol,0.15),  #iShares 7 – 10 Year Treasury ETF (IEF)
+            (self.AddEquity('GLD', Resolution.Daily).Symbol,0.075), #SPDR Gold Shares ETF (GLD),  #SPDR Gold Shares (GLD)  
+            (self.AddEquity('DBC', Resolution.Daily).Symbol,0.075) # PowerShares DB Commodity Index Tracking Fund (DBC)"
+            ]
             
-    def OnData(self, data):
-        pass
-
-    def Rebalance(self):
-        if self.IsWarmingUp: return
-        top3 = pd.Series(self.data).sort_values(ascending = False)[:3]
-        for kvp in self.Portfolio:
-            security_hold = kvp.Value
-           
-            if security_hold.Invested and (security_hold.Symbol.Value not in top3.index):
-                self.Liquidate(security_hold.Symbol)
+        self.Schedule.On(self.DateRules.MonthStart(self.etfs[0][0]), self.TimeRules.AfterMarketOpen(self.etfs[0][0]), self.Rebalance)
+        self.leverage = 1.5
+        self.monthCounter = 1
         
-        added_symbols = []        
-        for symbol in top3.index:
-            if not self.Portfolio[symbol].Invested:
-                added_symbols.append(symbol)
-        for added in added_symbols:
-            self.SetHoldings(added, 1/len(added_symbols))
+    def OnData(self, data):
+       pass
+     
+    def Rebalance(self):
+        if self.monthCounter is 12:
+            self.SetHoldings([PortfolioTarget(etf, target*self.leverage) for etf, target in self.etfs])
+            self.monthCounter = 1
+        else:
+            self.monthCounter = self.monthCounter+1
